61bbc53eaa7bbabe9a3655d0fdaa806ca3c23336
mokafuji820 <tfukae25@gmail.com>
2023-09-24 22:18:15 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,31 @@
+function post (){
+  const form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+};
+
+window.addEventListener('turbo:load', post);
+
+// function post (){
+
+//   const form = document.getElementById('form');
+//   form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//   function(e){
+//     e.preventDefault();
+//     const formData = new FormData(form);
+//     const XHR = new XMLHttpRequest();
+//     XHR.open('post', '/posts', true);
+//     XHR.responseType = 'json';
+//     XHR.send(formData);
+//   });
+
+// };
+
+// window.addEventListener('turbo:load', post);
