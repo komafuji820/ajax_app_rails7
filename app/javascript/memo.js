a74ffc7c6528a7874d3ea7c50825b43368e8fd1f
mokafuji820 <tfukae25@gmail.com>
2023-09-26 22:59:09 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+}
+
 function post (){
   const form = document.getElementById("form");
   form.addEventListener("submit", (e) => {
@@ -7,25 +21,17 @@ function post (){
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+    if (XHR.status != 200){
+      alert(`Error ${XHR.status}: ${XHR.statusText}`);
+      return null;
+    };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = '';
+    };
   });
 };
 
 window.addEventListener('turbo:load', post);
-
-// function post (){
-
-//   const form = document.getElementById('form');
-//   form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//   function(e){
-//     e.preventDefault();
-//     const formData = new FormData(form);
-//     const XHR = new XMLHttpRequest();
-//     XHR.open('post', '/posts', true);
-//     XHR.responseType = 'json';
-//     XHR.send(formData);
-//   });
-
-// };
-
-// window.addEventListener('turbo:load', post);
